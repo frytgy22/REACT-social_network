1a92d39515fb8a3c10bf35dcccb3e5a7e564193e
frytgy22@rambler.ru <>
2020-08-15 21:23:36 +0300

    add

---
+++ social-network/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSMPLYjcIkjkIBrCotJigoXpCDygj3jcUvzjQ&usqp=CAU'
+                alt='logo'/>
+        </header>
+    )
+}
+export default Header;
