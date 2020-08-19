9be035a8ed1bba95a447cff6bb1885f036d3e12a
frytgy22@rambler.ru <>
2020-08-19 21:39:26 +0300

    added route

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -4,20 +4,22 @@ import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Dialogs from "./components/dialogs/Dialogs";
 import Profile from "./components/profile/Profile";
-
+import {BrowserRouter, Route} from 'react-router-dom'
 
 const App = () => {
     return (
-        <div className={'app-wrapper'}>
-            <Header/>
-            <Navbar/>
-
-            <div className={'app-wrapper-content'}>
-                {/*<Profile/>*/}
-            <Dialogs/>
+        <BrowserRouter>
+            <div className={'app-wrapper'}>
+                <Header/>
+                <Navbar/>
+                <div className={'app-wrapper-content'}>
+                    <Route path={'/dialogs'} component={Dialogs}/>
+                    <Route path={'/profile'} component={Profile}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
 export default App;
+
