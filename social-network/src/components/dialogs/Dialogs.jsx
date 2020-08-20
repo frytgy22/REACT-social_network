64aa94da26b230db8540af0c5b23a231ade9a9ab
frytgy22@rambler.ru <>
2020-08-20 09:07:26 +0300

    added Navlink

--- social-network/src/components/dialogs/Dialogs.jsx
+++ social-network/src/components/dialogs/Dialogs.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import classes from "./Dialogs.module.css"
 
 
 const Dialogs = () => {
     return (
-        <div >
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' '+ classes.active}>
+                    User1
+                </div>
+                <div className={classes.dialog}>
+                    User2
+                </div>
+                <div className={classes.dialog}>
+                    User3
+                </div>
+                <div className={classes.dialog}>
+                    User4
+                </div>
+                <div className={classes.dialog}>
+                    User5
+                </div>
+            </div>
+
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>Hello</div>
+                <div className={classes.message}>Yo</div>
+            </div>
         </div>
     )
 }
--- social-network/src/components/navbar/Navbar.jsx
+++ social-network/src/components/navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import classes from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}><a href={'/profile'}>Profile</a></div>
-            <div className={`${classes.item} ${classes.active}`}><a href={'/dialogs'}>Messages</a></div>
+            <div className={classes.item}>
+                <NavLink to="/profile" activeClassName={classes.activeLink}>Profile</NavLink>
+            </div>
+
+            <div className={classes.item}>
+                <NavLink to="/dialogs" activeClassName={classes.activeLink}>Messages</NavLink>
+            </div>
+
             <div className={classes.item}><a>News</a></div>
             <div className={classes.item}><a>Music</a></div>
             <div className={classes.item}><a>Settings</a></div>
