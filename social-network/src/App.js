e213b83fac21ecec1da5ecca417f6ee22d460dea
frytgy22@rambler.ru <>
2020-08-25 22:12:02 +0300

    added ref

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -4,27 +4,25 @@ import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Dialogs from "./components/dialogs/Dialogs";
 import Profile from "./components/profile/Profile";
-import {BrowserRouter, Route} from 'react-router-dom'
+import {Route} from "react-router";
 
 const App = (props) => {
 
     return (
-        <BrowserRouter>
-            <div className={'app-wrapper'}>
-                <Header/>
-                <Navbar/>
-                <div className={'app-wrapper-content'}>
-                    {/*<Route path={'/dialogs'} component={Dialogs}/>*/}
-                    <Route
-                        path={'/dialogs'}
-                        render={() => <Dialogs state={props.state.messagesPage}/>}
-                    />
-                    <Route
-                        path={'/profile'}
-                        render={() => <Profile state={props.state.profilePage}/>}/>
-                </div>
+        <div className={'app-wrapper'}>
+            <Header/>
+            <Navbar/>
+            <div className={'app-wrapper-content'}>
+                {/*<Route path={'/dialogs'} component={Dialogs}/>*/}
+                <Route
+                    path={'/dialogs'}
+                    render={() => <Dialogs state={props.state.messagesPage}/>}
+                />
+                <Route
+                    path={'/profile'}
+                    render={() => <Profile state={props.state.profilePage}/>}/>
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
--- social-network/src/components/profile/my_posts/MyPosts.jsx
+++ social-network/src/components/profile/my_posts/MyPosts.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import classes from "./MyPosts.module.css";
 import Post from "./post/Post";
 
+let newPostElement = React.createRef();
+
+let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+}
 
 const MyPosts = (props) => {
-debugger;
+    debugger;
     let postsElements = props.posts.map(
         post => <Post message={post.message} likesCount={post.likesCount}/>
     );
@@ -14,10 +20,10 @@ debugger;
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
 
