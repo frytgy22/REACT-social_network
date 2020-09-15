7950565cf8b14ac534e068a33196ebf2fba7c5ab
frytgy22 <lebedevavaleria967@gmail.com>
2020-09-15 09:21:19 +0300

    added delete url

--- social-network/src/components/users/Users.jsx
+++ social-network/src/components/users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./users.module.css";
 import userPhoto from "../../assets/images/user.png";
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 let Users = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -31,10 +32,27 @@ let Users = (props) => {
                         <div>
                             {u.followed
                                 ? <button onClick={() => {
-                                    props.unfollow(u.id)
+
+                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/unfollow/${u.id}`, {
+                                        withCredentials: true
+                                    })
+                                        .then(response => {
+                                            if (response.data.resultCode === 0) {
+                                                props.unfollow(u.id)
+                                            }
+                                        })
+
                                 }}>Unfollow</button>
                                 : <button onClick={() => {
-                                    props.follow(u.id)
+
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                        withCredentials: true
+                                    })
+                                        .then(response => {
+                                            if (response.data.resultCode === 0) {
+                                                props.follow(u.id)
+                                            }
+                                        })
                                 }}>Follow</button>}
                         </div>
                     </span>
--- social-network/src/components/users/UsersContainer.jsx
+++ social-network/src/components/users/UsersContainer.jsx
@@ -19,7 +19,9 @@ class UsersContainer extends React.Component {
         this.props.toggleIsFetching(true)
 
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}
-        &count=${this.props.pageSize}`)
+        &count=${this.props.pageSize}`, {
+            withCredentials: true //TODO
+        })
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
@@ -32,7 +34,9 @@ class UsersContainer extends React.Component {
         this.props.toggleIsFetching(true)
 
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}
-        &count=${this.props.pageSize}`)
+        &count=${this.props.pageSize}`,{
+            withCredentials: true //TODO
+        })
             .then(response => {
                 this.props.toggleIsFetching(false)
                 this.props.setUsers(response.data.items)
